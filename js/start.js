bc9133edac12f70bac0a9a4dfdd90717b79c5ec5
mminsong <pamten@naver.com>
2024-03-29 16:54:51 +0900

    Second Day

--- js/start.js
+++ js/start.js
@@ -1,6 +1,18 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
+function addAnser() {
+  var a = document.querySelector(".answerBox");
+  var answer = document.createElement("Button");
+}
+
+function goNext(qIdx) {
+  var q = document.querySelector(".qBox");
+  q.innerHTML = qnaList[qIdx].q;
+  for (let i in qnaList[qIdx].a) {
+    addAnswer();
+  }
+}
 function begin() {
   main.style.WebkitAnimation = "fadeOut 1s";
   main.style.animation = "fadeOut 1s";
@@ -11,5 +23,8 @@ function begin() {
       main.style.display = "none";
       qna.style.display = "block";
     }, 450);
+    let qIdx = 0;
+    let aIdx = 0;
+    goNext(qIdx);
   }, 450);
 }
