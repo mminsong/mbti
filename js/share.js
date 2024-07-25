a69ce1e3910bf93366448cfc875ed6cadff05a42
mminsong <105476298+mminsong@users.noreply.github.com>
2024-07-25 11:43:33 +0900

    공유2

--- js/share.js
+++ js/share.js
@@ -1,35 +1,32 @@
-function kakaoShare() {
+const url = "https://toyproject-mbti.netlify.app/";
+function setShare() {
+  var resultImg = document.querySelector("#resultImg");
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = "십이간지  연애유형 결과";
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = ur;
+  +"img/image-" + resultAlt + ".png";
+  const shareURL = url + "page.result-" + resultAlt + ".html";
+
   Kakao.Link.sendDefault({
     objectType: "feed",
     content: {
-      title: "딸기 치즈 케익",
-      description: "#케익 #딸기 #삼평동 #카페 #분위기 #소개팅",
-      imageUrl:
-        "http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+      title: shareTitle,
+      description: shareDes,
+      imageUrl: shareImage,
       link: {
         // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
-        mobileWebUrl: "https://developers.kakao.com",
-        webUrl: "https://developers.kakao.com",
+        mobileWebUrl: shareURL,
+        WebUrl: shareURL,
       },
     },
-    social: {
-      likeCount: 286,
-      commentCount: 45,
-      sharedCount: 845,
-    },
+
     buttons: [
       {
-        title: "웹으로 보기",
+        title: "결과확인하기",
         link: {
-          mobileWebUrl: "https://developers.kakao.com",
-          webUrl: "https://developers.kakao.com",
-        },
-      },
-      {
-        title: "앱으로 보기",
-        link: {
-          mobileWebUrl: "https://developers.kakao.com",
-          webUrl: "https://developers.kakao.com",
+          mobileWebUrl: shareURL,
+          webUrl: shareURL,
         },
       },
     ],
