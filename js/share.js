0552f0f43b34382c16e7cc69ab7789c75ddc50ed
mminsong <105476298+mminsong@users.noreply.github.com>
2024-07-25 10:53:55 +0900

    7.25

---
+++ js/share.js
@@ -0,0 +1,37 @@
+function kakaoShare() {
+  Kakao.Link.sendDefault({
+    objectType: "feed",
+    content: {
+      title: "딸기 치즈 케익",
+      description: "#케익 #딸기 #삼평동 #카페 #분위기 #소개팅",
+      imageUrl:
+        "http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+      link: {
+        // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
+        mobileWebUrl: "https://developers.kakao.com",
+        webUrl: "https://developers.kakao.com",
+      },
+    },
+    social: {
+      likeCount: 286,
+      commentCount: 45,
+      sharedCount: 845,
+    },
+    buttons: [
+      {
+        title: "웹으로 보기",
+        link: {
+          mobileWebUrl: "https://developers.kakao.com",
+          webUrl: "https://developers.kakao.com",
+        },
+      },
+      {
+        title: "앱으로 보기",
+        link: {
+          mobileWebUrl: "https://developers.kakao.com",
+          webUrl: "https://developers.kakao.com",
+        },
+      },
+    ],
+  });
+}
